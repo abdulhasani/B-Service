e51f800dcc0533baaf74a9c1aee53a4ccbe7c891
BHasani <abdul.kadir.hasani@basajans.com>
2017-05-13 01:04:42 +0700

    push

---
+++ src/main/java/com/bebaryan/b/service/PersonService.java
@@ -0,0 +1,31 @@
+package com.bebaryan.b.service;
+
+import com.bebaryan.b.core.model.PersonModel;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author hasani
+ */
+@Path("/service/person")
+public class PersonService {
+    
+    @GET
+    @Path("/{requestId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonModel getSingle(@PathParam("requestId") String param){
+        PersonModel person = new PersonModel();
+        person.setRequestId(param);
+        person.setFullName("Abdul Kadir Hasani");
+        person.setFirstName("Abdul");
+        person.setMiddleName("Kadir");
+        person.setLastName("Hasani");
+        person.setAge(24);
+
+        return person;
+    }
+}
