c58fc99a01683f3d0732f0fc7c2f57c75bdbbc5e
BHasani <abdul.kadir.hasani@basajans.com>
2017-07-22 14:24:00 +0700

    testing service

--- src/main/java/com/bebrayan/ngapak/core/service/PersonService.java
+++ src/main/java/com/bebrayan/ngapak/core/service/PersonService.java
@@ -2,6 +2,8 @@ package com.bebrayan.ngapak.core.service;
 
 
 import com.bebrayan.ngapak.core.model.PersonModel;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -28,5 +30,41 @@ public class PersonService {
         person.setAge(24);
 
         return person;
+    }
+    
+    @GET
+    @Path("")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PersonModel> getList(){
+        List<PersonModel> models = new ArrayList<PersonModel>();
+        
+        PersonModel abdul = new PersonModel();
+        abdul.setRequestId("1");
+        abdul.setFullName("Abdul Kadir Hasani");
+        abdul.setFirstName("Abdul");
+        abdul.setMiddleName("Kadir");
+        abdul.setLastName("Hasani");
+        abdul.setAge(24);
+        models.add(abdul);
+        
+        PersonModel bridge = new PersonModel();
+        bridge.setRequestId("2");
+        bridge.setFullName("Bridge Hasani");
+        bridge.setFirstName("Bridge");
+        bridge.setMiddleName(null);
+        bridge.setLastName("Hasani");
+        bridge.setAge(24);
+        models.add(bridge);
+        
+        PersonModel joker = new PersonModel();
+        joker.setRequestId("3");
+        joker.setFullName("Joker Hasani");
+        joker.setFirstName("Joker");
+        joker.setMiddleName(null);
+        joker.setLastName("Hasani");
+        joker.setAge(24);
+        models.add(joker);
+        
+        return models;
     }
 }
